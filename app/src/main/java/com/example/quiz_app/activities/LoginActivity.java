8357bd677f8deac01bca9a29e86ce462b0e701ba
Rares <rares.tarabega@global-bit.com>
2019-05-18 19:23:21 +0300

    firebase login

---
+++ app/src/main/java/com/example/quiz_app/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.quiz_app.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.quiz_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
